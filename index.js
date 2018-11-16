a5919f4a663cd8ae724892d1ae4c9010f4254292
Austin <autbrown@ucsc.edu>
2018-11-16 14:43:58 -0500

    modified ffmpeg-static files

---
+++ index.js
@@ -0,0 +1,28 @@
+// Modified ffmpeg-static
+// Returns correct OS and Architecture
+
+// os now asks for window.require
+var os = window.require('os')
+var path = require('path')
+
+// Platform and arch are strings now
+var platform = os.platform + ''
+if (platform !== 'linux' && platform !== 'darwin' && platform !== 'win32') {
+  throw new Error('Unsupported platform.')
+}
+
+var arch = os.arch + ''
+if (platform === 'darwin' && arch !== 'x64') {
+  throw new Error('Unsupported architecture.')
+}
+
+var ffmpegPath = path.join(
+  'node_modules',
+  'ffmpeg-static',
+  'bin',
+  platform,
+  arch,
+  platform === 'win32' ? 'ffmpeg.exe' : 'ffmpeg'
+)
+
+exports.path = ffmpegPath;
